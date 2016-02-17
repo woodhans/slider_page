a7cc578cbe18bb0567dbc67605c44c89581b49e3
hans.wu <woodhans1@hotmail.com>
2016-02-17 17:42:20 +0800

    random

--- jquery.slider.js
+++ jquery.slider.js
@@ -80,11 +80,15 @@
 		Closed:null
 	}
 	function randomString(minlen,maxlen){
+		var _rnd;
 		maxlen=maxlen?maxlen:minlen;
-		str=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+		str=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",0,1,2,3,4,5,6,7,8,9];
 		_arr=[];
 		for(i=0;i<maxlen;i++){
+			if(i==0)
 			_rnd=Math.floor(Math.random()*26);
+			else
+			_rnd=Math.floor(Math.random()*str.length);
 			if(i>minlen){
 				if(Math.round(Math.random())){
 					return _arr.join('')
